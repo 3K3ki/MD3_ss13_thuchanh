eb14d3e4d15377e4fb5ab6292310b9ee118f0875
3K3ki <danghung090@gmail.com>
2023-03-23 20:19:16 +0700

    commit

---
+++ src/thuchanh1/Main.java
@@ -0,0 +1,16 @@
+package thuchanh1;
+
+import static com.sun.prism.impl.packrect.RectanglePacker.binarySearch;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(binarySearch(list, 2));  /* 0 */
+        System.out.println(binarySearch(list, 11)); /* 4 */
+        System.out.println(binarySearch(list, 79)); /*12 */
+        System.out.println(binarySearch(list, 1));  /*-1 */
+        System.out.println(binarySearch(list, 5));  /*-1 */
+        System.out.println(binarySearch(list, 80)); /*-     System.out.println(binarySearch(list, 2));  /* 0 */
+        System.out.println(binarySearch(list, 11)); /* 4 */
+        System.out.println(binarySearch(list, 79)); /*12 */
+        System.out.println(binarySearch(list, 1));  /*-1 */
+        System.out.println(binarySearch(list, 5));  /*-1 */
